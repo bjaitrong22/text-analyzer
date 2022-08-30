73bd696f37178860603d340dc1cd3cb58a1d0bc7
Bai Jaitrong <bjaitrong@comcast.net>
2022-08-29 21:50:19 -0600

    Add function to count number of occurence of a word.

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,15 @@ function wordCounter(text) {
   });
   return wordCount;
 }
+
+function numberOfOccurrencesInText(word,text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (word === element) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
